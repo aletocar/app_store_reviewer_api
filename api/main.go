bb18a62282e264aea2eea46fd672a22de5d03dcc
Alejandro Tocar <aletocar@hotmail.com>
2025-02-02 18:45:44 -0300

    added CORS

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"app_store_reviewer/cron"
 	"app_store_reviewer/utils"
 	"encoding/json"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
@@ -16,6 +17,11 @@ func main() {
 
 	cron.Run()
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowMethods: []string{"GET", "HEAD", "OPTIONS"},
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{"Origin"},
+	}))
 	router.GET("/apps", getApps)
 	router.GET("/apps/:id/reviews", getAppReviews)
 	router.GET("/health", health)
@@ -41,7 +47,20 @@ func getAppReviews(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, data)
 }
 func getApps(c *gin.Context) {
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "No apps have been found"})
+	file, _ := os.Open("./applications.json")
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+	var apps []string
+	byteValue, _ := io.ReadAll(file)
+	err := json.Unmarshal(byteValue, &apps)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+	}
+	c.IndentedJSON(http.StatusOK, apps)
 }
 func health(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "OK"})
